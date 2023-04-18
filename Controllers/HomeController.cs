85a5adedbbcedb4860d40fc83b68ea56286560bf
Fernando José Cabrera Ramírez <fcabrera@sb.gob.do>
2023-04-18 02:20:32 -0400

    Proyecto Glass

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Glass.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Glass.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+           var documents = Helpers.DocumentHelper.GetAllDocuments();
+
+            return View(documents);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string filter, bool match = true)
+        {
+            
+            var documents = filter != "" ? DocumentHelper.SearchDocuments(filter, match) : DocumentHelper.GetAllDocuments();
+
+            return View(documents);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Helpers/DocumentHelper.cs
@@ -0,0 +1,92 @@
+﻿using Glass.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace Glass.Helpers
+{
+    public class DocumentHelper
+    {
+        public static List<Document> SearchDocuments(string query, bool matchAll)
+        {
+            return matchAll ? MathAll(query) : MathAny(query);
+
+        }
+
+        public static List<Document> MathAny(string query)
+        {
+            List<Document> allDocuments = GetAllDocuments();
+            List<Document> documents = new List<Document>();
+            string[] words = (Regex.Replace(query, @"[^a-zA-Z0-9% ._]", string.Empty).ToLower()).Split(' ');
+
+            foreach (Document doc in allDocuments)
+            {
+
+                if (words.Any(w => Regex.Replace(doc.Content, @"[^a-zA-Z0-9% ._]", string.Empty).ToLower().Contains(w)))
+                    documents.Add(doc);
+            }
+
+            return documents;
+
+        }
+
+        public static List<Document> MathAll(string query)
+        {
+            List<Document> allDocuments = GetAllDocuments();
+            List<Document> documents = new List<Document>();
+            string[] words = (Regex.Replace(query, @"[^a-zA-Z0-9% ._]", string.Empty).ToLower()).Split(' ');
+
+            foreach(Document doc in allDocuments){
+
+                if (words.All(w => Regex.Replace(doc.Content, @"[^a-zA-Z0-9% ._]", string.Empty).ToLower().Contains(w)))
+                    documents.Add(doc);
+            }
+
+            return documents;
+
+        }
+
+        public static List<Document> GetAllDocuments()
+        {
+            List<Document> documents = new List<Document>();
+
+            OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["Default"].ConnectionString);
+
+            string commandText = "SELECT * FROM DOCUMENTS";
+
+            con.Open();
+
+            using (var Cmd = new OracleCommand(commandText, con))
+            {
+
+                using (var Reader = Cmd.ExecuteReader())
+                {
+                    if (Reader.HasRows)
+                    {
+                        while (Reader.Read())
+                        {
+
+                            documents.Add(new Document()
+                            {
+                                Id = Convert.ToInt32(Reader["Id"].ToString()),
+                                Title = Reader["Title"].ToString(),
+                                Content = Reader["Content"].ToString()
+                            }); ;
+                        }
+                    }
+                }
+
+            }
+
+            con.Close();
+
+            return documents;
+
+        }
+
+    }
+}
